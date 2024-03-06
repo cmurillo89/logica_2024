30e9aa2a1047fb59495fdd94635d42649e7b8c5e
cmurillo <christopherrmg@gmail.com>
2024-03-05 21:20:39 -0600

    fin primera clase

--- clase1.py
+++ clase1.py
@@ -1,1 +1,27 @@
-print("hola mundo")
+# TENEMOS 4 TIPOS DE VARIABLES, LAS CUALES SON: BOOLEANAS, NUMERICAS, CADENAS Y FLOTANTES
+estado = True
+nombre_completo = "Juan Perez Garcia"
+edad= 25
+estatura = 1.75
+
+print("El estado de la persona es: ", estado, "y su nombre completo es: ", nombre_completo, "y su edad es: ", edad, "y su estatura es: ", estatura)
+
+# VAMOS A TRABAJAR CON LOS OPERADORES ARITMETICOS
+
+# Suma
+numero1 = 5000
+numero2 = 3580
+suma = numero1 + numero2
+print("La suma es: ", suma)
+# Resta
+resta = 5 - 5
+print("La resta es: ", resta)
+# Multiplicacion
+multiplicacion = 5 * 5
+print("La multiplicacion es: ", multiplicacion)
+# Division
+division = 5 / 5
+print("La division es: ", division)
+# Modulo
+modulo = 5 % 5
+print("El modulo es: ", modulo)
